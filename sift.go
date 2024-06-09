14c4e7f1e913e67c4738e9bf10b2ac8c392f2b35
Matt Armstrong <matt@rfc20.org>
2024-06-09 14:57:35 -0700

    WIP: checkpoint repro

--- sift.go
+++ sift.go
@@ -6,21 +6,17 @@ import (
 	"log/slog"
 	"os"
 	"path/filepath"
-	"slices"
-	"strings"
 
 	"github.com/charmbracelet/bubbles/help"
 	"github.com/charmbracelet/bubbles/key"
-	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
-	"github.com/davecgh/go-spew/spew"
-	"github.com/ghodss/yaml"
-	"github.com/matta/sift/internal/replicatedtodo"
 )
 
 type teaModel struct {
-	wrapped *model
+	help         *help.Model
+	windowWidth  int
+	windowHeight int
 }
 
 // Init implements tea.Model.
@@ -29,61 +25,18 @@ func (outer teaModel) Init() tea.Cmd {
 	return nil
 }
 
-// Update implements tea.Model.
-func (outer teaModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	slog.Debug(fmt.Sprintf("teaModel.Update: '%+v' ENTER", spew.Sdump(msg)))
-	cmd := outer.wrapped.update(msg)
-	slog.Debug(fmt.Sprintf("teaModel.Update: '%+v' LEAVE", spew.Sdump(msg)))
-	return outer, cmd
-}
-
-// View implements tea.Model.
-func (outer teaModel) View() string {
-	slog.Debug("View() ENTER")
-	out := outer.wrapped.view()
-	slog.Debug("View() LEAVE")
-	return out
-}
-
-type model struct {
-	help            *help.Model
-	persisted       *replicatedtodo.Model
-	textInput       *textinput.Model
-	windowWidth     int
-	windowHeight    int
-	acceptTextInput func(title string)
-	cursorID        string
-}
-
-func newModel() *model {
+func newModel() teaModel {
 	help := help.New()
-	textInput := textinput.New()
-	return &model{
-		help:            &help,
-		persisted:       replicatedtodo.New(),
-		textInput:       &textInput,
-		acceptTextInput: nil,
-		cursorID:        "",
+	return teaModel{
+		help: &help,
 	}
-}
-
-func (m *model) addSampleItems() {
-	m.newTodo("todo 1")
-	m.newTodo("todo 2")
 }
 
 // keyMap holds a set of keybindings. To work for help it must satisfy
 // key.Map. It could also very easily be a map[string]key.Binding.
 type keyMap struct {
-	Up     key.Binding
-	Down   key.Binding
-	Toggle key.Binding
-	Add    key.Binding
-	Edit   key.Binding
-	Help   key.Binding
-	Quit   key.Binding
-	Cancel key.Binding
-	Accept key.Binding
+	Help key.Binding
+	Quit key.Binding
 }
 
 // ShortHelp returns keybindings to be shown in the mini help view. It's part
@@ -96,32 +49,11 @@ func (k keyMap) ShortHelp() []key.Binding {
 // key.Map interface.
 func (k keyMap) FullHelp() [][]key.Binding {
 	return [][]key.Binding{
-		{k.Up, k.Down, k.Toggle, k.Add}, // first column
-		{k.Help, k.Quit},                // second column
+		{k.Help, k.Quit},
 	}
 }
 
 var keys = keyMap{
-	Up: key.NewBinding(
-		key.WithKeys("up", "k"),
-		key.WithHelp("↑/k", "move up"),
-	),
-	Down: key.NewBinding(
-		key.WithKeys("down", "j"),
-		key.WithHelp("↓/j", "move down"),
-	),
-	Toggle: key.NewBinding(
-		key.WithKeys("x"),
-		key.WithHelp("x", "toggle item"),
-	),
-	Add: key.NewBinding(
-		key.WithKeys("a"),
-		key.WithHelp("a", "add item"),
-	),
-	Edit: key.NewBinding(
-		key.WithKeys("e"),
-		key.WithHelp("e", "edit item"),
-	),
 	Help: key.NewBinding(
 		key.WithKeys("?"),
 		key.WithHelp("?", "toggle help"),
@@ -130,68 +62,27 @@ var keys = keyMap{
 		key.WithKeys("q", "esc", "ctrl+c"),
 		key.WithHelp("q", "quit"),
 	),
-	Cancel: key.NewBinding(key.WithKeys("esc")),
-	Accept: key.NewBinding(
-		key.WithKeys("enter")),
 }
 
-func (m *model) newTodo(title string) {
-	m.persisted.NewTodo(title)
-}
-
-func (m *model) save() error {
-	bytes, err := yaml.Marshal(&m.persisted)
-	if err != nil {
-		return fmt.Errorf("failed to marshal model: %w", err)
-	}
-
-	PERM := 0600
-	err = os.WriteFile(UserDataFile(), bytes, os.FileMode(PERM))
-	if err != nil {
-		return fmt.Errorf("failed to save model: %w", err)
-	}
-
-	return nil
-}
-
-func updateTextInput(input *textinput.Model, msg tea.Msg) tea.Cmd {
-	temp, cmd := input.Update(msg)
-	*input = temp
-
-	return cmd
-}
-
-func (m *model) update(msg tea.Msg) tea.Cmd {
+func (m teaModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
 		// If we set a width on our sub-models, so they can respond as needed.
 		m.help.Width = msg.Width
-		m.textInput.Width = msg.Width
 		m.windowWidth = msg.Width
 		m.windowHeight = msg.Height
 	case tea.KeyMsg:
-		return m.handleKeyMsg(msg)
+		cmd := m.handleKeyMsg(msg)
+		return m, cmd
 	}
 
-	return nil
+	return m, nil
 }
 
-func (m *model) handleKeyMsg(msg tea.KeyMsg) tea.Cmd {
+func (m teaModel) handleKeyMsg(msg tea.KeyMsg) tea.Cmd {
 	switch {
-	case m.textInput.Focused():
-		return m.handleFocusedTextInput(msg)
 	case key.Matches(msg, keys.Help):
 		m.help.ShowAll = !m.help.ShowAll
-	case key.Matches(msg, keys.Up):
-		m.cursorUp()
-	case key.Matches(msg, keys.Down):
-		m.cursorDown()
-	case key.Matches(msg, keys.Toggle):
-		m.toggle()
-	case key.Matches(msg, keys.Add):
-		return m.add()
-	case key.Matches(msg, keys.Edit):
-		return m.edit()
 	case key.Matches(msg, keys.Quit):
 		return tea.Quit
 	}
@@ -199,208 +90,16 @@ func (m *model) handleKeyMsg(msg tea.KeyMsg) tea.Cmd {
 	return nil
 }
 
-func (m *model) toggle() {
-	id := m.currentItem().ID
-	m.persisted.ToggleDone(id)
-	slog.Debug("persisted after toggling %s\n%s", id, m.persisted.DebugString())
-}
-
-func (m *model) add() tea.Cmd {
-	m.acceptTextInput = func(title string) {
-		m.newTodo(title)
-		m.disableTextInput()
-		m.acceptTextInput = nil
-	}
-
-	return m.textInput.Focus()
-}
-
-func (m *model) edit() tea.Cmd {
-	m.acceptTextInput = func(title string) {
-		if len(title) > 0 {
-			m.persisted.SetTitle(m.currentItem().ID, title)
-			m.disableTextInput()
-		}
-
-		m.acceptTextInput = nil
-	}
-	m.textInput.SetValue(m.currentItem().Title)
-
-	return m.textInput.Focus()
-}
-
-func findID(items []replicatedtodo.Item, cursorID string) int {
-	index := slices.IndexFunc(items, func(item replicatedtodo.Item) bool {
-		return item.ID == cursorID
-	})
-
-	return index
-}
-
-func (m *model) currentItem() replicatedtodo.Item {
-	items := m.loadItems()
-	index := findID(items, m.cursorID)
-
-	return items[index]
-}
-
-func (m *model) cursorDown() {
-	items := m.loadItems()
-	index := findID(items, m.cursorID)
-
-	switch {
-	case len(items) == 0:
-		m.cursorID = ""
-	case index >= 0 && index < len(items)-1:
-		m.cursorID = items[index+1].ID
-	default:
-		m.cursorID = items[0].ID
-	}
-}
-
-func (m *model) cursorUp() {
-	items := m.loadItems()
-	index := findID(items, m.cursorID)
-
-	switch {
-	case len(items) == 0:
-		m.cursorID = ""
-	case index > 0:
-		m.cursorID = items[index-1].ID
-	default:
-		m.cursorID = items[len(items)-1].ID
-	}
-}
-
-func (m *model) handleFocusedTextInput(msg tea.KeyMsg) tea.Cmd {
-	switch {
-	case key.Matches(msg, keys.Cancel):
-		m.disableTextInput()
-	case key.Matches(msg, keys.Accept):
-		m.accept()
-	default:
-		return updateTextInput(m.textInput, msg)
-	}
-
-	return nil
-}
-
-func (m *model) accept() {
-	m.acceptTextInput(m.textInput.Value())
-	m.acceptTextInput = nil
-	m.disableTextInput()
-}
-
-func (m *model) disableTextInput() {
-	m.textInput.Reset()
-	m.textInput.Blur()
-}
-
-func (m *model) view() string {
-	return m.viewOld()
-}
-
-func (m *model) viewOld() string {
-	// The size of the screen is unknown before the first tea.WindowSizeMsg.
-	// In this case don't render.
-	if m.windowWidth == 0 || m.windowHeight == 0 {
-		return "Hello!"
-	}
-
-	out := ""
-
-	items := m.loadItems()
-
-	for _, item := range items {
-		cursor := " "
-		if item.ID == m.cursorID {
-			cursor = ">"
-		}
-
-		done := " "
-		if item.State == "checked" {
-			done = "x"
-		}
-
-		out += fmt.Sprintf("%s [%s] %s\n", cursor, done, item.Title)
-	}
-
-	if m.textInput.Focused() {
-		out += m.textInput.View()
-	} else {
+func (m teaModel) View() string {
+	out := fmt.Sprint("The screen has ", m.windowWidth, " columns and ", m.windowHeight, " rows\n")
+	if m.windowWidth > 0 {
 		out += m.help.View(keys)
-	}
 
-	style := lipgloss.NewStyle().BorderStyle(lipgloss.NormalBorder())
-	out = style.Render(out)
+		style := lipgloss.NewStyle().BorderStyle(lipgloss.NormalBorder())
+		out = style.Render(out)
+	}
 
 	return out
-}
-
-func (m *model) viewNew() string {
-	{
-		// The size of the screen is unknown before the first tea.WindowSizeMsg.
-		// In this case don't render.
-		if m.windowWidth == 0 || m.windowHeight == 0 {
-			return ""
-		}
-
-		var out strings.Builder
-
-		if m.textInput.Focused() {
-			out.WriteString(m.textInput.View())
-		} else {
-			slog.Debug("m.help.View()")
-			help := m.help.View(keys)
-			helpHeight := lipgloss.Height(help)
-
-			itemsHeight := m.windowHeight - helpHeight - 2
-
-			var itemsOut strings.Builder
-			items := m.loadItems()
-
-			for i, item := range items {
-				if i > itemsHeight {
-					break
-				}
-				cursor := " "
-				if item.ID == m.cursorID {
-					cursor = ">"
-				}
-
-				done := " "
-				if item.State == "checked" {
-					done = "x"
-				}
-
-				itemsOut.WriteString(
-					fmt.Sprintf("%s [%s] %s\n", cursor, done, item.Title))
-			}
-
-			out.WriteString(
-				lipgloss.NewStyle().Height(itemsHeight).Render(itemsOut.String()))
-			out.WriteString(help)
-		}
-
-		style := lipgloss.NewStyle().
-			Width(m.windowWidth - 2).
-			Height(m.windowHeight - 2).
-			BorderStyle(lipgloss.NormalBorder())
-
-		return style.Render(out.String())
-	}
-}
-
-func (m *model) loadItems() []replicatedtodo.Item {
-	items := m.persisted.GetAllItems()
-	items = slices.DeleteFunc(items, func(item replicatedtodo.Item) bool {
-		return item.State == "removed"
-	})
-	slices.SortFunc(items, func(i, j replicatedtodo.Item) int {
-		return strings.Compare(i.ID, j.ID)
-	})
-
-	return items
 }
 
 func UserHomeDir() string {
@@ -414,36 +113,6 @@ func UserHomeDir() string {
 
 func UserDataFile() string {
 	return filepath.Join(UserHomeDir(), ".sift.yaml")
-}
-
-func loadModel() *teaModel {
-	model := loadPersistedModel()
-
-	return &teaModel{model}
-}
-
-func loadPersistedModel() *model {
-	model := newModel()
-
-	bytes, err := os.ReadFile(UserDataFile())
-	if err != nil {
-		log.Printf("Failed to read model file: %v", err)
-		model.addSampleItems()
-
-		return model
-	}
-
-	var replicatedModel replicatedtodo.Model
-	if err = yaml.Unmarshal(bytes, &replicatedModel); err != nil {
-		log.Printf("Failed to unmarshal model file: %v", err)
-		model.addSampleItems()
-
-		return model
-	}
-
-	model.persisted = &replicatedModel
-
-	return model
 }
 
 func setUpLogging() *os.File {
@@ -472,16 +141,12 @@ func main() {
 	}()
 	slog.Info("program started")
 
-	teaModel := loadModel()
+	teaModel := newModel()
 
 	program := tea.NewProgram(teaModel, tea.WithAltScreen())
 	_, err := program.Run()
 	if err != nil {
 		slog.Error("Error running program: %v", err)
-	}
-
-	if err := teaModel.wrapped.save(); err != nil {
-		slog.Error("Error saving: %v", err)
 	}
 
 	slog.Debug("program exiting")
