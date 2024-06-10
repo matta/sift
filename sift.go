77e421cca261f94334cc55f6c5b170715b83dd1d
Matt Armstrong <matt@rfc20.org>
2024-06-09 18:30:14 -0700

    WIP: finish repro

--- sift.go
+++ sift.go
@@ -5,122 +5,55 @@ import (
 	"log"
 	"log/slog"
 	"os"
-	"path/filepath"
 
-	"github.com/charmbracelet/bubbles/help"
-	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/davecgh/go-spew/spew"
 )
 
-type teaModel struct {
-	help         *help.Model
-	windowWidth  int
-	windowHeight int
+type model struct {
+	width  int
+	height int
 }
 
 // Init implements tea.Model.
-func (outer teaModel) Init() tea.Cmd {
+func (outer model) Init() tea.Cmd {
 	slog.Debug("teaModel.Init()")
 	return nil
 }
 
-func newModel() teaModel {
-	help := help.New()
-	return teaModel{
-		help: &help,
-	}
-}
-
-// keyMap holds a set of keybindings. To work for help it must satisfy
-// key.Map. It could also very easily be a map[string]key.Binding.
-type keyMap struct {
-	Help key.Binding
-	Quit key.Binding
-}
-
-// ShortHelp returns keybindings to be shown in the mini help view. It's part
-// of the key.Map interface.
-func (k keyMap) ShortHelp() []key.Binding {
-	return []key.Binding{k.Help, k.Quit}
-}
-
-// FullHelp returns keybindings for the expanded help view. It's part of the
-// key.Map interface.
-func (k keyMap) FullHelp() [][]key.Binding {
-	return [][]key.Binding{
-		{k.Help, k.Quit},
-	}
-}
-
-var keys = keyMap{
-	Help: key.NewBinding(
-		key.WithKeys("?"),
-		key.WithHelp("?", "toggle help"),
-	),
-	Quit: key.NewBinding(
-		key.WithKeys("q", "esc", "ctrl+c"),
-		key.WithHelp("q", "quit"),
-	),
-}
-
-func (m teaModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	prefix := spew.Sprintf("Update(%#v)", msg)
 	slog.Debug(fmt.Sprintf("%s ENTER", prefix))
 	defer slog.Debug(fmt.Sprintf("%s LEAVE", prefix))
 
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
-		// If we set a width on our sub-models, so they can respond as needed.
-		m.help.Width = msg.Width
-		m.windowWidth = msg.Width
-		m.windowHeight = msg.Height
+		m.width = msg.Width
+		m.height = msg.Height
 	case tea.KeyMsg:
-		cmd := m.handleKeyMsg(msg)
-		return m, cmd
+		return m, tea.Quit
 	}
 
 	return m, nil
 }
 
-func (m teaModel) handleKeyMsg(msg tea.KeyMsg) tea.Cmd {
-	switch {
-	case key.Matches(msg, keys.Help):
-		m.help.ShowAll = !m.help.ShowAll
-	case key.Matches(msg, keys.Quit):
-		return tea.Quit
-	}
-
-	return nil
-}
-
-func (m teaModel) View() string {
+func (m model) View() string {
 	slog.Debug("View() ENTER")
 	defer slog.Debug("View() LEAVE")
 
-	out := fmt.Sprint("The screen has ", m.windowWidth, " columns and ", m.windowHeight, " rows\n")
-	if m.windowWidth > 0 {
-		out += m.help.View(keys)
+	out := fmt.Sprint("The screen has ", m.width,
+		" columns and ", m.height, " rows\n")
 
-		style := lipgloss.NewStyle().BorderStyle(lipgloss.NormalBorder())
+	if m.width > 0 {
+		style := lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{
+			Light: "#DDDADA",
+			Dark:  "#9C9C00",
+		})
 		out = style.Render(out)
 	}
 
 	return out
-}
-
-func UserHomeDir() string {
-	usr, err := os.UserHomeDir()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return usr
-}
-
-func UserDataFile() string {
-	return filepath.Join(UserHomeDir(), ".sift.yaml")
 }
 
 func setUpLogging() *os.File {
@@ -150,9 +83,7 @@ func main() {
 	}()
 	slog.Info("program started")
 
-	teaModel := newModel()
-
-	program := tea.NewProgram(teaModel, tea.WithAltScreen())
+	program := tea.NewProgram(model{}, tea.WithAltScreen())
 	_, err := program.Run()
 	if err != nil {
 		slog.Error("Error running program: %v", err)
